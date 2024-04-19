e94fae00e9bb4673401069237eeef963398e5e09
bhuvavivek <vivekpate26@gmail.com>
2024-04-19 11:17:55 +0530

    adding a profileimage api

--- modules/shared/controllers/imageControllers.js
+++ modules/shared/controllers/imageControllers.js
@@ -1,15 +1,35 @@
+const UserProfileService = require("../../user/services/userProfileService");
+
+const VendorService = require("../../vendor/services/vendorservice");
 const { uploadMiddleware } = require("../middleware/uploadmiddleware");
-const PasswordService = require("../services/password");
+const ImageService = require("../services/imageService");
 
 class ImageController {
   async uploadImageProfile(req, res, next) {
-    uploadMiddleware(req.type).single("file")(req, res, function (err) {
-      if (err) {
-        return next(err);
+    let user;
+    try {
+      if (req.type === "VENDOR") {
+        user = await VendorService.getVendorById(req.user.id);
       }
-      console.log(req.file);
-      return res.status(200).json({ status: true, message: req.file });
-    });
+
+      if (req.type === "USER") {
+        user = await UserProfileService.getUserProfile(req.user.id);
+      }
+
+      uploadMiddleware(req.type).single("file")(req, res, async function (err) {
+        if (err) {
+          throw err;
+        }
+        const result = await ImageService.addProfileImage(
+          req.type,
+          req.file,
+          user
+        );
+        return res.status(200).json({ status: true, message: result });
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 }
 module.exports = new ImageController();
---
+++ modules/shared/services/imageService.js
@@ -0,0 +1,70 @@
+const User = require("../../user/models/user");
+const Vendor = require("../../vendor/models/vendor");
+const { extractPublicId } = require("../middleware/uploadmiddleware");
+const cloudinary = require("../../../config/cloudineryConfig");
+// imageService.js
+class ImageService {
+  async addProfileImage(type, file, userEntity) {
+    // add profile image
+    let entityModel;
+    let entityProfile;
+    let entity;
+
+    try {
+      if (!file) {
+        const error = new Error("No file uploaded");
+        error.statusCode = 400;
+        throw error;
+      }
+      if (type === "VENOR") {
+        entityModel = Vendor;
+        entityProfile = "vendorProfile";
+        entity = "vendor";
+      }
+      if (type === "USER") {
+        entityModel = User;
+        entityProfile = "userProfile";
+        entity = "user";
+      }
+
+      if (!userEntity[entity][entityProfile]) {
+      }
+
+      // delete old image
+      let folderName;
+      if (type === "USER") {
+        folderName = "UserProfile";
+      }
+      if (type === "VENDOR") {
+        folderName = "VendorProfile";
+      }
+      const publicId = extractPublicId(
+        folderName,
+        userEntity[entity][entityProfile]
+      );
+
+      console.log(publicId);
+
+      const deleteProfileImage = await cloudinary.uploader.destroy(publicId, {
+        invalidate: true,
+        resource_type: "image",
+      });
+
+      console.log(deleteProfileImage);
+      // const EntityProfile = await entityModel.findByIdAndUpdate(
+      //   userEntity[entity]._id,
+      //   {
+      //     [entityProfile]: file.path,
+      //   }
+      // );
+
+      // console.log(EntityProfile);
+
+      return file;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+module.exports = new ImageService();
